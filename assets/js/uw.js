d1813a589762c5386d3945407394b3d0d953b6e4
Taher Shihadeh <taher@unixwars.com>
2024-09-14 10:36:10 +0100

    Cleanup

--- assets/js/uw.js
+++ assets/js/uw.js
@@ -1,24 +1,35 @@
-function main() {
-    function makeImages(size) {
+const RandomSlides = (containerId, size, miliseconds) => {
+    function makeImages(
+        size,
+        prefix = 'assets/img/emblem_',
+        extension = '.webp',
+        alt = "Random Sigil",
+        title = "Emblem"
+    ) {
+        let pad = String(size).length;
         return [...Array(size).keys()].map(i => {
-            let src = 'assets/img/emblem_' +('0' + i).slice(-2)+'.webp';
             let img = document.createElement('img');
-            img.src = src;
-            img.alt = "Random Sigil";
-            img.title = "Emblem";
+            img.src = prefix + String(i).padStart(pad,'0') + extension;
+            img.alt = alt;
+            img.title = title;
+            img.style.opacity = 0;
+            img.style.transition="opacity 2s ease-in-out";
             return img;
-    })};    
-    function switchImage() {
-        images[idx].classList.toggle('show');
-        idx = Math.floor(Math.random() * images.length);
-        images[idx].classList.toggle('show');
+        })
     }
-    let div = document.getElementById('content');
-    let images = makeImages(49);
+    function switchImage() {
+        images[idx].style.opacity = 1 - images[idx].style.opacity;
+        idx = Math.floor(Math.random() * images.length);
+        images[idx].style.opacity = 1 - images[idx].style.opacity;
+    }
+    let div = document.getElementById(containerId);
+    let images = makeImages(size);
     let idx = Math.floor(Math.random() * images.length);
-    images[idx].classList.toggle('show');
+    images[idx].style.opacity = 1;
     div.append(...images);
-    setInterval(switchImage, 5000);
+    return setInterval(switchImage, miliseconds);
 }
 
-window.onload = (event) => main();
+window.onload = (event) => {
+    RandomSlides('content', 49, 3000);
+}
